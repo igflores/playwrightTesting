b4fcd9e3131adf317e6279a40f52f23bdd5e67d9
igflores <139146133+igflores@users.noreply.github.com>
2025-09-11 18:50:53 -0500

    feat(auth): add AuthUtils and helper directory structure

---
+++ tests/helper/auth/AuthUtils.ts
@@ -0,0 +1,24 @@
+import { Page } from "@playwright/test";
+import { LoginPage } from "../../pages/LoginPage";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export class AuthUtils {
+    private page: Page;
+    private loginPage: LoginPage;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.loginPage = new LoginPage(page);
+    }
+
+    async login(username?: string, password?: string) {
+        await this.loginPage.navigateToLoginPage();
+        await this.loginPage.login(
+            username || process.env.USER_NAME || "",
+            password || process.env.USER_PASSWORD || ""
+        );
+        await this.loginPage.validateSuccessfulLogin();
+    }
+}
